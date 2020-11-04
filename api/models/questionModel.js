32dff054655e18560cd0f09896f387bf9956e466
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-04 18:15:58 +0330

    update models

--- api/models/questionModel.js
+++ api/models/questionModel.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const questionSchema = new mongoose.Schema({
+const questionSchema = new mongoose.Schema({    
     question: {
         type: String,
         required: [true, 'Question can not be empty'],
-        minlength: [80, 'Qestion can not be lower than 80 char'],
-        trim: true
     },
     answers: [
         {
