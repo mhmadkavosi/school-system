b079946a5cd4ba63d76d3c407beafe32f66aac7d
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-07 18:38:37 +0330

    add get all question

--- api/controllers/questionController.js
+++ api/controllers/questionController.js
@@ -19,7 +19,30 @@ exports.addQuestion = async (req, res, next) => {
   }
   next();
 };
+
+exports.getAllQuestion = async (req, res, next) => {
+  try {
+    const doc = await Question.find();
+
+    if (!doc)
+      return res.status(404).json({
+        message: "There is no document yet",
+      });
+    res.status(200).json({
+      status: "success",
+      question: {
+        doc,
+      },
+    });
+    next();
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: "Some Thing went wrong",
+      error,
+    });
             }
+};
         });
     } catch (error) {
         res.status(500).json({ error });
