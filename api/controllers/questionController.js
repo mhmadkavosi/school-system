9e908dc8e458170ab8279d32de8098ce45b9ee84
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-07 19:00:20 +0330

    add update question

--- api/controllers/questionController.js
+++ api/controllers/questionController.js
@@ -1,88 +1,108 @@
 const Question = require("./../models/questionModel");
 
-exports.addQuestion = async (req, res, next) => {
-  try {
-    const doc = await Question.create(req.body);
-    res.status(201).json({
-      status: "success",
-      question: {
-        doc,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: "Some Thing went wrong",
-      error,
-    });
-  }
-  next();
+exports.addQuestion = async(req, res, next) => {
+    try {
+        const doc = await Question.create(req.body);
+        res.status(201).json({
+            status: "success",
+            question: {
+                doc,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: "Some Thing went wrong",
+            error,
+        });
+    }
+    next();
 };
 
-exports.getAllQuestion = async (req, res, next) => {
-  try {
-    const doc = await Question.find();
+exports.getAllQuestion = async(req, res, next) => {
+    try {
+        const doc = await Question.find();
 
-    if (!doc)
-      return res.status(404).json({
-        message: "There is no document yet",
-      });
-    res.status(200).json({
-      status: "success",
-      question: {
-        doc,
-      },
-    });
-    next();
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: "Some Thing went wrong",
-      error,
-    });
-  }
+        if (!doc)
+            return res.status(404).json({
+                message: "There is no document yet",
+            });
+        res.status(200).json({
+            status: "success",
+            question: {
+                doc,
+            },
+        });
+        next();
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: "Some Thing went wrong",
+            error,
+        });
+    }
 };
 
-exports.getOneQuestion = async (req, res, next) => {
-  try {
-    const doc = await Question.findById(req.params.id);
+exports.getOneQuestion = async(req, res, next) => {
+    try {
+        const doc = await Question.findById(req.params.id);
 
-    // TODO send 404 error
+        // TODO send 404 error
 
-    res.status(200).json({
-      status: "success",
-      doc,
-    });
-    next();
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: "Some Thing went wrong",
-      error,
-    });
-  }
+        res.status(200).json({
+            status: "success",
+            doc,
+        });
+        next();
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: "Some Thing went wrong",
+            error,
+        });
+    }
 };
 
-exports.deleteOneQuestion = async (req, res, next) => {
-  try {
-    const doc = await Question.findByIdAndDelete(req.params.id);
+exports.updateQuestion = async(req, res, next) => {
+    try {
+        const doc = await Question.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: "success",
+            doc
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: "some Thing went wrong",
+            error
+        })
+    }
+}
 
-    if (!doc)
-      return res.status(404).json({
-        status: "fail",
-        message: "There is no document with that ID",
-      });
 
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-    next();
-  } catch (error) {
-    res.status(500).json({
-      status: "fail",
-      message: "Some Thing went wrong",
-      error,
-    });
-  }
+exports.deleteOneQuestion = async(req, res, next) => {
+    try {
+        const doc = await Question.findByIdAndDelete(req.params.id);
+
+        if (!doc)
+            return res.status(404).json({
+                status: "fail",
+                message: "There is no document with that ID",
+            });
+
+        res.status(204).json({
+            status: "success",
+            data: null,
+        });
+        next();
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: "Some Thing went wrong",
+            error,
+        });
+    }
 };
