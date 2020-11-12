8103d4f21938dfca895d01d3c35a915a778b4ef5
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-12 19:32:38 +0330

    set errors for prod and development

--- api/controllers/errorController.js
+++ api/controllers/errorController.js
@@ -1,10 +1,41 @@
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+}
+
+const sendErrorProd = (err, res) => {
+    // Operational , trusted error : send message to the clint
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+        // Programing or other unkown error : dont't leak error details
+    } else {
+        // 1) log error
+        console.error('ERROR !!!', err);
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: "error",
+            message: "Something went very wrong!!"
+        })
+    }
+}
+
+
 // Create error Middleware (global handler midlleware)
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    }
 }
