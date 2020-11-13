8749c7a0f6376bc577cd858dada80fcb2df7c489
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-13 15:55:45 +0330

    update error handlers

--- api/utils/appError.js
+++ api/utils/appError.js
@@ -1,16 +1,26 @@
-class AppError extends Error {
+// class AppError extends Error {
+//     constructor(message, statusCode) {
+//         super(message);
+
+//         // initlize status code
+//         this.statusCode = statusCode;
+//         // Check if status code start with  4 like 404 status is fail if is not it's error 
+//         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+//         // Check for error is Operational error or not (it's for application not bugs or programer errors)
+//         this.isOperational = true;
+//         // return a string response of where the location of error in code
+//         Error.captureStackTrace(this, this.constructor);
+//     }
+// }
+
+// module.exports = AppError;
+
+
+class ErrorRespons extends Error {
     constructor(message, statusCode) {
         super(message);
-
-        // initlize status code
         this.statusCode = statusCode;
-        // Check if status code start with  4 like 404 status is fail if is not it's error 
-        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-        // Check for error is Operational error or not (it's for application not bugs or programer errors)
-        this.isOperational = true;
-        // return a string response of where the location of error in code
-        Error.captureStackTrace(this, this.constructor);
     }
 }
 
-module.exports = AppError;
+module.exports = ErrorRespons;
