bdeafc3e6f8b83e2181f297ac3fe9a3e65d42d0a
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-16 21:01:33 +0330

    user controller

---
+++ api/controllers/userController.js
@@ -0,0 +1,24 @@
+const User = require('../models/userModel');
+const asyncHandler = require('../utils/asyncHandler');
+const errorResponse = require('../utils/errorResponse');
+
+exports.addUser = asyncHandler(async(req, res, next) => {
+    const doc = await User.create(req.body);
+    res.status(201).json({
+        status: "success",
+        User: {
+            doc,
+        },
+    });
+});
+
+
+exports.getAllUser = asyncHandler(async(req, res, next) => {
+    const doc = await User.find();
+    res.status(200).json({
+        status: "success",
+        User: {
+            doc,
+        },
+    });
+});
