76e3ce56bcf931f5fd91a443c1908e8bbe58d852
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-18 16:27:45 +0330

    delete some comments

--- api/utils/errorResponse.js
+++ api/utils/errorResponse.js
@@ -1,21 +1,3 @@
-// class errorResponse extends Error {
-//     constructor(message, statusCode) {
-//         super(message);
-
-//         // initlize status code
-//         this.statusCode = statusCode;
-//         // Check if status code start with  4 like 404 status is fail if is not it's error 
-//         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-//         // Check for error is Operational error or not (it's for application not bugs or programer errors)
-//         this.isOperational = true;
-//         // return a string response of where the location of error in code
-//         Error.captureStackTrace(this, this.constructor);
-//     }
-// }
-
-// module.exports = errorResponse;
-
-
 class ErrorRespons extends Error {
     constructor(message, statusCode) {
         super(message);
