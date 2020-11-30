c4a10f134f293a73b6624f804309c00ad13ba859
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-30 16:23:05 +0330

    register controller

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -3,9 +3,16 @@ const asyncHandler = require('../utils/asyncHandler');
 const User = require('../models/userModel');
 
 // @desc Register User 
-// @route   /api/v1/auth/register
+// @route   /api/v1/auth/register POST
 // @access public
 
 exports.register = asyncHandler(async (req, res, next) => {
+    const { name, email, password, role } = req.body;
+
+    // Create User in database 
+    const user = await User.create({
+        name, email, password, role
+    });
+
     res.status(201).json({ success: true });
 })
