8e290beed1f8653aeaf372ecca1043b25cb5cd12
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-30 16:24:47 +0330

    add register route

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router
-    .route('/')
+    .route('/register')
     .post(authController.register);
 
 
