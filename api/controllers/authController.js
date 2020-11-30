e54626060362d845cdbbd0f3463430190467b06e
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-30 17:07:07 +0330

    login func

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -19,3 +19,33 @@ exports.register = asyncHandler(async (req, res, next) => {
 
     res.status(201).json({ success: true , token });
 })
+
+
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email & password 
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Check if password matches 
+    const isMatch = user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Create token
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
+});
