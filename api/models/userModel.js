d50df00cdd0a762e5375e76cfa277be1c7ec2461
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-11-30 16:24:30 +0330

    hash user password with bcrypt

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -60,7 +61,13 @@ const userSchema = new mongoose.Schema({
         timestamps:true
     }
 );
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password ,salt);
+
 });
+
 
 
 const User = mongoose.model("User", userSchema);
