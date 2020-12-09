fd0f16096d43480d3e582c27cc3cc380483374e5
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-09 20:09:24 +0330

    Get reset password token route

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.route('/register').post(authController.register);
 router.route('/login').post(authController.login);
-
 router.route('/me').get(authController.protect, authController.getMe);
+router.route('/forgotPassword').post(authController.forgotPassword);
 
 module.exports = router;
