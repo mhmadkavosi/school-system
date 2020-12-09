339839101051a55e41339996068a9e567d4462fb
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-09 20:09:14 +0330

    Get reset password token controller

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -122,3 +122,22 @@ exports.login = asyncHandler(async (req, res, next) => {
   // Create token
   sendTokenResponse(user, 200, res);
 });
+
+// @desc Get reset password token
+// @route POST /api/v1/auth/forgotPassword
+// @access Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email.', 404));
+  }
+
+  // get reset token
+
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({ success: true, data: user });
+});
