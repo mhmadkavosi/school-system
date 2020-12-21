e17cba7d8bc521217f3dc1399f5537ef1163d1f5
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-21 18:38:48 +0330

    upate user password route

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -9,6 +9,10 @@ router.route('/me').get(authController.protect, authController.getMe);
 router
   .route('/updatedetails')
   .put(authController.protect, authController.updateDetails);
+router
+  .route('/updatepassword')
+  .put(authController.protect, authController.updatePassword);
+
 router.route('/forgotpassword').post(authController.forgotPassword);
-router.route('/resetPassword/:resetToken').put(authController.resetPassword);
+router.route('/resetpassword/:resetToken').put(authController.resetPassword);
 module.exports = router;
