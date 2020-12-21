115de0e73bfc33421c0bdf0c3834be66efd81835
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-21 20:04:55 +0330

    user crud only for admin

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,23 +1,54 @@
 const User = require('../models/userModel');
 const asyncHandler = require('../utils/asyncHandler');
 
-exports.addUser = asyncHandler(async (req, res, next) => {
-  const doc = await User.create(req.body);
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
   res.status(201).json({
     status: 'success',
-    User: {
-      doc,
+    data: {
+      user,
     },
   });
 });
 
-exports.getAllUser = asyncHandler(async (req, res, next) => {
-  const doc = await User.find();
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  const user = await User.find();
   res.status(200).json({
     status: 'success',
-    count: doc.length,
-    Useres: {
-      doc,
+    count: user.length,
+    data: {
+      user,
     },
+  });
+});
+
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
+
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    datas: {
+      user,
+    },
+  });
+});
+
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.id);
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
