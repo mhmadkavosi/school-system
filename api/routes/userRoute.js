e59c7e987e56406100eae8742515f52f3468792c
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-21 20:05:04 +0330

    add user route

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,15 +1,19 @@
 const express = require('express');
 
-const UserController = require('../controllers/userController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route('/').get(UserController.getAllUser).post(UserController.addUser);
+router.use(authController.protect);
+router.use(authController.authorize('admin'));
 
-// router
-//     .route('/:id')
-//     .get(UserController.getOneUser)
-//     .delete(UserController.deleteOneUser)
-//     .patch(UserController.updateUser);
+router.route('/').get(userController.getUsers).post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .delete(userController.deleteUser)
+  .put(userController.updateUser);
 
 module.exports = router;
