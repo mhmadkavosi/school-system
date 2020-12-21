21ad335154f47bcd6a88ad067bc3777c42ac0154
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-21 18:37:51 +0330

    update user details

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -80,6 +80,22 @@ exports.authorize = (...role) => (req, res, next) => {
   next();
 };
 
+// @desc Update user details
+// @route PUT /api/v1/auth/updatedetails
+// @access Privete
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
 // @desc Register User
 // @route   /api/v1/auth/register POST
 // @access public
