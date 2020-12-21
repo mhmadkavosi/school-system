3021d56adf1aaa5ce915d08af44fed39409679f0
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-21 17:07:37 +0330

    fix typeo

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.route('/register').post(authController.register);
 router.route('/login').post(authController.login);
 router.route('/me').get(authController.protect, authController.getMe);
-router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/forgotpassword').post(authController.forgotPassword);
 
 module.exports = router;
