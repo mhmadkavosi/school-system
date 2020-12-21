28ef43f34a71a0e1017f884fc002c2ecb31cae92
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2020-12-21 18:01:00 +0330

    reset password route

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -7,5 +7,5 @@ router.route('/register').post(authController.register);
 router.route('/login').post(authController.login);
 router.route('/me').get(authController.protect, authController.getMe);
 router.route('/forgotpassword').post(authController.forgotPassword);
-
+router.route('/resetPassword/:resetToken').put(authController.resetPassword);
 module.exports = router;
